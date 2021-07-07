6bc10c96dbbf0598387103e35087b05cc9b53a11
Hugo Gicquel <hugogicquel@me.com>
2021-07-07 09:37:25 +0200

    init

---
+++ ex03/ft_sort_params.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgicquel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 09:36:48 by hgicquel          #+#    #+#             */
+/*   Updated: 2021/07/07 09:36:49 by hgicquel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*t;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 0;
+		while (j < argc - i - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				t = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = t;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	ft_sort(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+			j++;
+		write(1, argv[i], j);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
